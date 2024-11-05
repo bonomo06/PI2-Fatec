9c77db987f5a0779b1089f467f8046ce3bb7d8ca
bonomo06 <pedrohbnm@gmail.com>
2024-11-05 08:44:06 -0300

    commit para erro no cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express from 'express'
 import routes from './routes'
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+app.use(cors({
+    origin: 'http://localhost:3000' // ou use '*' para permitir todas as origens temporariamente
+}));
 
 app.use(express.json())
 
